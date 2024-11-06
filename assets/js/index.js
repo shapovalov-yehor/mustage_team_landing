383ea301669e3facb243ba1128dc24ffa6193579
dmitriypiskun <piskun.dmitriy@hotmail.com>
2024-11-06 18:56:19 +0200

    feat: add other blocks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,21 @@
-const repeatContainer = document.querySelector(".repeating-container");
+const repeatContainers = document.querySelectorAll(".repeating-container");
 const repeatText = document.querySelector(".repeating-text");
-const text = "mustage.team / ";
+const text = "mustage.team";
 
 const windowWidth = window.innerWidth;
 const repeatCount = windowWidth / 195;
 /**
  * TODO: Fix this
  */
-for (let i = 0; i < repeatCount; i++) {
-  const textElement = document.createElement("div");
-  textElement.className = "repeating-text";
-  textElement.textContent = text;
-  repeatContainer.appendChild(textElement);
-}
+repeatContainers.forEach((repeatContainer, index) => {
+  for (let i = 0; i < repeatCount; i++) {
+    const textElement = document.createElement("div");
+    textElement.className = !index ? "repeating-text" : "repeating-text text-light";
+    textElement.textContent = text;
+    repeatContainer.appendChild(textElement);
+  }
+});
+
+const countViews = document.getElementById("countView");
+const countBotUsers = document.getElementById("countBotUsers");
+const countGraduate = document.getElementById("countGraduate");
